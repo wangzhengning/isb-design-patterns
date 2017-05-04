d5267f237266f90e977596f42d65c3a9f44402ca
wangzhengning <a1256761520@163.com>
2017-05-04 14:19:43 +0800

    create the isb-design-patterns

--- src/test/java/com/designpatterns/example/part01/DuckTest.java
+++ src/test/java/com/designpatterns/example/part01/DuckTest.java
@@ -1,7 +1,10 @@
 package com.designpatterns.example.part01;
 
+import com.designpatterns.example.part01.fly.FlyNoWay;
 import com.designpatterns.example.part01.fly.FlyWithWings;
+import com.designpatterns.example.part01.quack.MuteQuack;
 import com.designpatterns.example.part01.quack.Quack;
+import com.designpatterns.example.part01.quack.Squeak;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +17,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:/META-INF.spring/spring-shell-plugin.xml"})
 public class DuckTest {
-
-    
 
     @Autowired
     private DecoyDuck decoyDuck;
@@ -32,8 +33,8 @@ public class DuckTest {
     @Test
     public void testDecoyDuck(){
         //测试诱饵鸭子
-        decoyDuck.setFlyBehavior ( new FlyWithWings () );
-        decoyDuck.setQuackBehavior ( new Quack ());
+        decoyDuck.setFlyBehavior ( new FlyNoWay () );
+        decoyDuck.setQuackBehavior ( new Squeak ());
 
         decoyDuck.display ();
         decoyDuck.performFly ();
@@ -44,17 +45,35 @@ public class DuckTest {
     @Test
     public void testMallardDuck(){
         //测试绿头鸭
+        mallardDuck.setFlyBehavior ( new FlyWithWings () );
+        mallardDuck.setQuackBehavior ( new Quack ());
+
+        mallardDuck.display ();
+        mallardDuck.performFly ();
+        mallardDuck.performQuack ();
+
     }
 
     @Test
     public void testReadHeadDuck(){
         //测试红头鸭
+        readHeadDuck.setFlyBehavior ( new FlyWithWings () );
+        readHeadDuck.setQuackBehavior ( new MuteQuack ());
 
+        readHeadDuck.display ();
+        readHeadDuck.performFly ();
+        readHeadDuck.performQuack ();
     }
 
     @Test
     public void testRubberDuck(){
         //测试橡皮鸭子
+        rubberDuck.setFlyBehavior ( new FlyNoWay () );
+        rubberDuck.setQuackBehavior ( new MuteQuack ());
+
+        rubberDuck.display ();
+        rubberDuck.performFly ();
+        rubberDuck.performQuack ();
     }
 
 }
